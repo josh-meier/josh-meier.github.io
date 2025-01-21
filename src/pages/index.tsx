3b733d682339fe0fe001e14b0240892df9e9f17d
josh-meier <joshmeier456@gmail.com>
2025-01-20 22:06:04 -0600

    trying to fix deployment, changed basepath and added important true

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { useState } from "react";
 // import '../styles/navbar.css'
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
